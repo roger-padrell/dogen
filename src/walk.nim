cfe67128006c330fe8ea3d2e67147099a8ba608c
Roger Padrell Casar <padrell.roger@gmail.com>
2025-03-09 12:16:48 +0100

    Add "fullContent" formatting

--- src/walk.nim
+++ src/walk.nim
@@ -11,12 +11,17 @@ type Param = object
     default*: string = "!exists"
 
 type
+    Returns* = object
+        description*: string
+        typeName*: string
+
     WalkItem* = object
         ## An item in the documentation
         name*: string
         fullContent*: string = ""
+        body*: string = ""
         description*: string = ""
-        returns*: string = ""
+        returns*: Returns = Returns()
         params*: Table[string, Param] = initTable[string, Param]()
         example*: string = ""
 
@@ -39,16 +44,92 @@ proc countLeadingSpaces(s: string): int =
     else:
         break
 
+proc removeWhiteSpace(s: var string) = 
+    s.removePrefix(" ".repeat(s.countLeadingSpaces()))
+
+proc removeWhiteSpace(st: string): string = 
+    var s = st;
+    s.removePrefix(" ".repeat(s.countLeadingSpaces()))
+    return s;
+
 proc formatFullContent(res: var WalkCont) = 
-    discard
+    ## Format a WalkCont
+    for item in res.items.keys:
+        res.items[item].fullContent.removePrefix("\n")
+        res.items[item].fullContent.removeSuffix("\n")
+        res.items[item].fullContent.removeWhiteSpace()
+        var lineN = 0;
+        var inExample = false;
+        for line in res.items[item].fullContent.split("\n"):
+            # Remove the DocComent
+            var noComSeq: seq[string] = line.split("##");
+            noComSeq.del(0)
+            let withoutComment = noComSeq.join("##")
+            let withoutIndent = withoutComment.removeWhiteSpace();
+
+            # Set as description if it's first line
+            if lineN == 0:
+                res.items[item].description = withoutIndent;
+
+            # Add to body if it does not start with "@" (it's not an object marker)
+            if not (withoutIndent.startsWith("@")) and not inExample:
+                if res.items[item].body != "":
+                    res.items[item].body = res.items[item].body & "\n" & withoutIndent
+                else:
+                    res.items[item].body = withoutIndent;
+
+            # Add to example if "inExample" is true and it does not start with "@eox"
+            if not (withoutIndent.startsWith("@eox")) and inExample:
+                if res.items[item].example != "":
+                    res.items[item].example = res.items[item].example & "\n" & withoutComment
+                else:
+                    res.items[item].example = withoutComment;
+
+            # Turn "inExample" on/off
+            if withoutIndent.startsWith("@eox"):
+                inExample = false;
+            if withoutIndent.startsWith("@example"):
+                inExample = true;
+                # Add "@example" to body
+                if res.items[item].body != "":
+                    res.items[item].body = res.items[item].body & "\n@example"
+                else:
+                    res.items[item].body = "@example";
+
+            # Add returns description
+            if withoutIndent.startsWith("@returns"):
+                inExample = true;
+                # Add "@returns" to body
+                if res.items[item].body != "":
+                    res.items[item].body = res.items[item].body & "\n@returns"
+                else:
+                    res.items[item].body = "@returns";
+                # Add data to "returns"
+                res.items[item].returns.description = withoutIndent.split("@returns")[1]
+
+            # Add parameter description
+            if withoutIndent.startsWith("@param"):
+                let paramName = withoutIndent.split("@param")[1].split(" ")[1]
+                inExample = true;
+                # Add "@param+name" to body
+                if res.items[item].body != "":
+                    res.items[item].body = res.items[item].body & "\n@param" & paramName
+                else:
+                    res.items[item].body = "@param" & paramName;
+                # Add data to "param[name]"
+                res.items[item].params[paramName].description = withoutIndent.split(paramName)[1]
+
+            # Update lineN
+            lineN = lineN + 1;
 
 proc generateJSON*(filePath: string): JsonNode = 
     ## This function generates the JSON object with the pre-render content
-    ## @params filePath The path to the file that will be readed and converted to JSON documentation
+    ## @param filePath The path to the file that will be readed and converted to JSON documentation
     ## @returns The JSON Object
     ## @example
     ## # Import the module...
     ## echo generateJSON("test1.nim")
+    ## @eox
     if not fileExists(filePath):
         echo &"File {filePath} does not exist"
         return;
@@ -68,21 +149,23 @@ proc generateJSON*(filePath: string): JsonNode =
             indentCount = " ".repeat(f.countLeadingSpaces())
             break;
     # Create the objects in the indent
+    var n = -1;
     for f in fileLines:
+        n = n + 1;
         # Update "current" and "indent" if necessary
         # Remove item from indent
         if (f.countLeadingSpaces()/indentCount.len()) < float(current):
             let toRemove = float(current) - (f.countLeadingSpaces()/indentCount.len())
             for i in 0..int(toRemove):
-                if indent[current].name == "*multipleTypeGen*":
-                    multipleTypes=false;
-                current = current - 1;
-                indent.shrink(current+1)
+                if indent[current].name == "*multipleTypeGen*" and (("=" in fileLines[n+2] and (not ("proc" in fileLines[n+2]) and not ("let " in fileLines[n+2]) and not ("var " in fileLines[n+2]) and not ("const " in fileLines[n+2]))) or ("=" in fileLines[n+1] and (not ("proc" in fileLines[n+1]) and not ("let " in fileLines[n+1]) and not ("var " in fileLines[n+1]) and not ("const " in fileLines[n+1])))):
+                    break;
+                else:
+                    current = current - 1;
+                    indent.shrink(current+1)
 
         # Remove indent
         var removedIndent = f;
-        removedIndent.removePrefix(" ".repeat(removedIndent.countLeadingSpaces()))
-        removedIndent.removeSuffix(" ")
+        removedIndent.removeWhiteSpace()
         # Check if it provoques a change of indent
         if removedIndent.startsWith("if"):
             indent.add(ILevel(name:"if", kind:"if"))
@@ -96,6 +179,7 @@ proc generateJSON*(filePath: string): JsonNode =
         elif removedIndent.startsWith("proc"):
             indent.add(ILevel(name:removedIndent.split(" ")[1].split("(")[0], kind:"proc"))
             current = current + 1;
+            # Parse params
             var params: Table[string, Param] = initTable[string, Param]()
             let betweenParenthesis = removedIndent.split("(")[1].split(")")[0]
             let paramString = betweenParenthesis.split(",")
@@ -103,12 +187,20 @@ proc generateJSON*(filePath: string): JsonNode =
                 for p in paramString:
                     let pa = p.split(":")
                     if "=" in pa[1]:
-                        params[pa[0]] = Param(name: pa[0], typeName: pa[1].split("=")[0], default: pa[1].split("=")[1])
+                        params[pa[0]] = Param(name: pa[0].removeWhiteSpace(), typeName: pa[1].split("=")[0].removeWhiteSpace(), default: pa[1].split("=")[1].removeWhiteSpace())
                     else:
-                        params[pa[0]] = Param(name: pa[0], typeName: pa[1])
+                        params[pa[0]] = Param(name: pa[0].removeWhiteSpace(), typeName: pa[1].removeWhiteSpace())
 
-                res.items[indent[current].name] = WalkItem(name:indent[current].name, params:params)
-                addedItems.add(indent[current].name)
+            # Parse return type
+            var afterPArr = removedIndent.split(")")
+            afterPArr.del(0);
+            let afterP = afterPArr.join(")")
+            var returnType = "";
+            if ":" in afterP:
+                returnType = afterP.split(":")[1].split("=")[0]
+            # Add as WalkItem
+            res.items[indent[current].name] = WalkItem(name:indent[current].name, params:params, returns: Returns(typeName:returnType))
+            addedItems.add(indent[current].name)
         elif removedIndent.startsWith("type") and len(removedIndent.split(" ")) > 1:
             indent.add(ILevel(name:removedIndent.split(" ")[1], kind:"type"))
             current = current + 1;
@@ -130,7 +222,7 @@ proc generateJSON*(filePath: string): JsonNode =
                 res.items[indent[current].name].fullContent = f;
                 addedItems.add(indent[current].name)
 
-        # Format the "fullContent" into specialized properties
-        res.formatFullContent()
+    # Format the "fullContent" into specialized properties
+    res.formatFullContent()
 
     return %* res
