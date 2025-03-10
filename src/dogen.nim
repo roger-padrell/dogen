ad1c434c8952538cdd6a404d2bf149c2c6e52325
Roger Padrell Casar <padrell.roger@gmail.com>
2025-03-10 16:52:35 +0100

    Add help command in base file

--- src/dogen.nim
+++ src/dogen.nim
@@ -1,8 +1,32 @@
-import os, genmd, genraw
+import os, genmd, genraw, strutils
 var
   arguments = commandLineParams()
 
-let command = arguments[0]
+proc help() = 
+  echo """
+Welcome to DOGEN
+
+DOGEN is a DOcumentation GENerator in written in, and for, the NIM programming language.
+
+Usage:
+  dogen                           | Displays this help message
+
+  dogen help                      | Displays this help message
+
+  dogen json someFile.nim         | Generates the DOGEN.json file for that nim file
+
+  dogen md someFile.nim           | Generates a markdown documentation for that nim file
+
+  dogen md someFile.dogen.json    | Generates the markdown representation of an already-created dogen.json file
+
+"""
+
+var command = ""
+if arguments.len() > 0:
+  command = arguments[0]
+else:
+  help()
+  quit(0)
 
 proc red(msg: string, error=true) = 
   echo "\e[31m"&msg&"\e[0m";
@@ -19,11 +43,19 @@ proc getFilePath(minArguments:int=2):string =
   else:
     red("Error: Some arguments are missing")
 
+if command == "help":
+  help()
+  quit(0)
 if command == "json":
   let filePath = getFilePath();
   echo genRaw(filePath)
 elif command == "md":
   let filePath = getFilePath();
-  green genRaw(filePath)
+  if filePath.endsWith(".dogen.json"):
+    green DOGENtoMD(filePath)
+  elif filePath.endsWith(".nim"):
+    green NIMtoMD(filePath)
+  else:
+    red "File type not compatible. You can only generate MarkDown from .dogen.json or nim files"
 else:
   red("Error: Command not found")
